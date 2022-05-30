3a0827da4a19d4a5fe0cac6a31f6db6f136060bd
Luqman Zaceria <luqmanzaceria@gmail.com>
2022-05-30 09:57:52 -0700

    Navbar links active when clicked

---
+++ activeState.js
@@ -0,0 +1,11 @@
+let links = document.querySelectorAll('.nav-link');
+for(let i=0; i<links.length; i++){
+  links[i].addEventListener('click', function() {
+    for(let j=0; j<links.length; j++)
+      links[j].classList.remove('active');
+    this.classList.add('active');
+  });
+}
+
+
+
